468b815cf990a2c7783366d0f24e373c69ba5184
kirankumar <140248017+Kirankumar0718@users.noreply.github.com>
2023-09-27 16:07:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-//your code here
-
+let articleless=[];
+let regexp=/\ba\b|\ban\b|\bthe\b/gi;
+let arr= ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+for (let i=0;i<arr.length;i++)
+	{
+		let articlelessitem =arr.replace(regexp,"");
+		articleless.push(articlelessitem);
+	}
+console.log(articleless);
